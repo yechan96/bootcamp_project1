695bf2c9134aca3ec39fcea97e00844b7fd6b1dd
ink <eric.zmo2@gmail.com>
2020-02-16 19:04:49 -0800

    display booksearch algo, get user current location algo initial submit git add . :wq

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -11,8 +11,68 @@ $(document).ready(function(){
     var bookSearchTitle = "";
     var location ="";
 
-    $(".searchButton").on("click",function(){
+    //asks user for current location and save location and weather info.
+    navigator.geolocation.getCurrentPosition(success, error)
 
+    $(".searchButton").on("click",function(){
+        $(".wrapperPg2").empty()
+
+        console.log(location)
+        console.log(weather)
+        bookSearchTitle =  $("#bkSearchInp").val();
+        $("#bkSearchInp").val("");
+        if(location == ""){
+            location = $("#locationInp").val();
+            $("#locationInp").val("");
+        }
+
+        var bkApiUrl = "https://openlibrary.org/search.json?q=";
+        var searchTemp = bookSearchTitle.replace(/ /g, "+");
+        var query = bkApiUrl + searchTemp;
+
+        $.ajax({
+            url: query,
+            method: "GET",
+            success: function(response){
+                libraryArr = response.docs;
+                console.log(libraryArr);
+            
+                var indexCounter = 0;
+
+                //loop to get 10 results to display from the array
+                for(i = 0; i<10; i++){
+                    var arryExist = false; 
+                        //move to next result if author, title or publish year is missing. 
+                        while(arryExist == false){
+                            if (libraryArr[indexCounter]["author_name"] === undefined || libraryArr[indexCounter]["title"] === undefined || libraryArr[indexCounter]["first_publish_year"] === undefined || libraryArr[indexCounter]["cover_i"] === undefined){
+                                indexCounter++;
+                            }
+                            else{
+                                arryExist = true;
+                            }
+                        }
+
+                    //makes the new html elements and appends them to display results
+                    var newBtn = $("<button class='button columns bkResults' value='" + indexCounter + "'>");
+                    var fgTemp = $("<figure class='column is-2'>");
+                    var imgTemp = $("<img class='bkImg' src='https://covers.openlibrary.org/b/id/" + libraryArr[indexCounter].cover_i + "-M.jpg'>");
+                    var divTemp = $("<div class='bkInfo column'>");
+                    var strTitle = $("<strong class='bkTitle'>").text(libraryArr[indexCounter].title);
+                    var authorTemp = $("<p class='bkElements'>").text("By: " + libraryArr[indexCounter].author_name[0]);
+                    var yearTemp = $("<p class='bkElements'>").text("Publish Year: " + libraryArr[indexCounter].first_publish_year);
+                    
+                        divTemp.append(strTitle);
+                        divTemp.append(authorTemp);
+                        divTemp.append(yearTemp);
+                        fgTemp.append(imgTemp);
+                        newBtn.append(fgTemp);
+                        newBtn.append(divTemp);
+                        $(".wrapperPg2").append(newBtn);
+                        indexCounter++;
+                }
+            }
+        })
+       
 
         hideExcept([".homeButton",".page2"]);
     })
@@ -30,6 +90,33 @@ $(document).ready(function(){
         hideExcept([".homeButton",".page2"]);
     })
 
+
+    //***************FUNCTION within Scope  *************/
+
+    //function if successfuly obtained user geolocation
+    function success(position) {
+        var currentLat = "lat=" + position.coords.latitude
+        var currentLon = "lon=" + position.coords.longitude
+        var geoCoorUrl = "https://api.openweathermap.org/data/2.5/weather?"
+        var apiKey = "&units=imperial&appid=1a6304f914f966e5dc4a8226a424190d" 
+        var queryURL = geoCoorUrl + currentLat + "&" + currentLon + apiKey
+        $.ajax({
+            url: queryURL,
+            method: "GET",
+            success: function(response){
+                weather = response
+                location = response.name
+                countryCode = response.sys.country
+                $(".searchButton").show()
+            }
+        })  
+    }
+
+    //function if failed to obtain user geolocation. Display location input for user to manually enter
+    function error() {
+        $("#locationInp").show()
+        $(".searchButton").show()
+    }
 })
 
 function hideExcept(array){
@@ -50,3 +137,6 @@ function hideExcept(array){
 //         $("#countryOptions").append(tempCountry);
 //     }
 // }
+
+
+
