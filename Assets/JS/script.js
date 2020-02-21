f282c47c19c4407f7172130908676eb252c738c0
Ashely <arumbaoa10@gmail.com>
2020-02-20 18:40:15 -0800

    New Assignment

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -84,14 +84,14 @@ $(document).ready(function(){
     function getWeather(){
         var weatherURL = "https://api.openweathermap.org/data/2.5/weather?q=" + location+ "&units=imperial&appid=f56c94699b79bf806441d23eacbfa401"
         $.get(weatherURL, function(param){
-       console.log("success callback")
-       weather.name=param.name
-       weather.state=param.weather[0].description
-       weather.temp=param.main.temp
-       weather.wind=param.wind.speed
-       weather.icon=param.weather[0].icon
-       console.log("weatcher",weather)
-   })
+            console.log("success callback")
+            weather.name=param.name
+            weather.state=param.weather[0].description
+            weather.temp=param.main.temp
+            weather.wind=param.wind.speed
+            weather.icon=param.weather[0].icon
+            console.log("weatcher",weather)
+        })
 
     }
     //function if successfuly obtained user geolocation
